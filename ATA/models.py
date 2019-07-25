dd0bab0a009d2c3a1ee7ee51d6c77c812d0fadd7
ammar2621 <ammar@alterra.id>
2019-07-25 21:25:46 +0700

    home

--- ATA/models.py
+++ ATA/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Artikel(models.Model):
+    foto = models.CharField(max_length=100)
+    judul = models.CharField(max_length=100)
+    tanggal = models.DateField()
+    isi = models.TextField()
+    kategori = models.CharField(max_length=100, default='0000000')
