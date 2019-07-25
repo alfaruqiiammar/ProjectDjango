a8fb81c3cf1d83fbe0a5174f75d57f18cbaf03ca
resita-alterra <resita@alterra.id>
2019-07-25 20:16:25 +0700

    Halaman Search utama

--- ATA/models.py
+++ ATA/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class VocabsIndo(models.Model):
+    indo = models.CharField(max_length=50)
+    arti = models.TextField()
+    related = models.TextField(null=True)
+    sinonim = models.TextField(null=True)
--- ATA/views.py
+++ ATA/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import VocabsIndo
 
 # Create your views here.
 def home(request):
     return render(request,'base/base.html')
+
+def search(request):
+    if request.method == 'GET':
+        ini = request.GET.get('cari')
+        if ini is not None:
+            arti = VocabsIndo.objects.get(indo=ini)
+            return render(request, 'searchkata.html', {'kata':ini,'arti': arti})
+    return render(request,'search.html')
+
+# def searchkata(request, kata):
+#     if request.method == 'GET':
+#         ini = request.GET.get('cari')
+#         return render(request, 'searchkata.html', {'kata':ini})
+#     return render(request, 'searchkata.html', {'kata': kata})
